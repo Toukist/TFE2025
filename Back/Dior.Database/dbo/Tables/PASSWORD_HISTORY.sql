1c6a41b9f32e3b0ce378d8fe6e9eaaa0006029d5
Toukist <154629202+Toukist@users.noreply.github.com>
2025-08-20 01:51:15 +0200

    maj

--- Back/Dior.Database/dbo/Tables/PASSWORD_HISTORY.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[PASSWORD_HISTORY] (
-    [Id]           BIGINT         IDENTITY (1, 1) NOT NULL,
-    [UserId]       BIGINT         NOT NULL,
-    [PasswordHash] NVARCHAR (200) NOT NULL,
-    [CreatedAt]    DATETIME       DEFAULT (getdate()) NOT NULL,
-    PRIMARY KEY CLUSTERED ([Id] ASC),
-    CONSTRAINT [FK_PasswordHistory_User] FOREIGN KEY ([UserId]) REFERENCES [dbo].[USER] ([ID])
-);
-
--- Back/Dior.Database/dbo/Tables/Team.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Team] (
-    [Id]          INT            IDENTITY (1, 1) NOT NULL,
-    [Name]        NVARCHAR (100) NOT NULL,
-    [Description] NVARCHAR (255) NULL,
-    [CreatedAt]   DATETIME       DEFAULT (getdate()) NULL,
-    PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
